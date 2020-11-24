5f476429d8de828ea1c4fca28dc0a89e639461d1
Hassan Juma <okothhassanjuma@gmail.com>
2020-11-24 21:51:26 +0300

    add models

--- news/models.py
+++ news/models.py
@@ -61,3 +61,6 @@ class MoringaMerch(models.Model):
     name = models.CharField(max_length=40)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20)
+
+    def __str__(self):
+        return self.name
