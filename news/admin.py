b664fa3958579aca4d01f346fb7234eadb06f1b8
Hassan Juma <okothhassanjuma@gmail.com>
2020-11-24 17:12:16 +0300

    add admin

--- news/admin.py
+++ news/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Editor,Article,tags
+from .models import Editor,Article,tags,MoringaMerch
 
 # Register your models here.
 
@@ -7,5 +7,6 @@ class ArticleAdmin(admin.ModelAdmin):
     filter_horizontal =('tags',)
 
 admin.site.register(Editor)
-admin.site.register(Article)
+admin.site.register(Article,ArticleAdmin)
+admin.site.register(MoringaMerch)
 admin.site.register(tags)
